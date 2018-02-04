2e2d6623efeea85246437d026662c6d73587b12e
colkar99 <colkar99@gmail.com>
2018-02-04 22:14:57 +0530

    created seed file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |x|
+	Blog.create(
+		title: "my blog post #{x}",
+		body: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+	)
+end
+puts "10 blog posts created"
+
+5.times do |x|
+	Skill.create(
+		title: "Rails #{x}",
+		percent_utilized: 15
+	)
+end
+puts "5 skills created"
+9.times do |x|
+	Portfolio.create(
+		title:"Portfolio title: #{x}",
+		subtitle: "",
+		body:"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est lab",
+		main_image:"http://via.placeholder.com/350x200",
+		thumb_image:"http://via.placeholder.com/600x400",
+	)
+end
+puts "9 portfolio created"
