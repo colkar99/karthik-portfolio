43704c038027ca357fa6c7b09592cb2247300c92
colkar99 <colkar99@gmail.com>
2018-02-05 21:11:29 +0530

    edit and update features are added

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -20,6 +20,21 @@ class PortfoliosController < ApplicationController
 	      end
 	    end
   	end
+  	def edit
+  		@portfolio_items = Portfolio.find(params[:id])
+  	end
+  	def update
+  		@portfolio_items = Portfolio.find(params[:id])
+	    respond_to do |format|
+	      if @portfolio_items.update(params.require(:portfolio).permit(:title,:subtitle, :body))
+	        format.html { redirect_to portfolios_path, notice: 'Blog was successfully updated.' }
+	        format.json { render :show, status: :ok, location: @blog }
+	      else
+	        format.html { render :edit }
+	        format.json { render json: @portfolio_items.errors, status: :unprocessable_entity }
+	      end
+	    end
+  	end
   	# def show
   	# 	@portfolio_item = Portfolio.find(params[:id])
   	# end
