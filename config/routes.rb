281780d51a995b1302e38002aea476c8e8062d74
colkar99 <colkar99@gmail.com>
2018-02-06 22:07:12 +0530

    merge the toggle feature with enum fun

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   get 'about-me',to: 'pages#about'
   get 'contact',to: 'pages#contact'
   root to: 'pages#home'
-  resources :blogs
+  resources :blogs do
+  	member do |variable|
+  		get :toggle_status
+  	end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
